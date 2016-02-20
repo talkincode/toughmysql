561a59c28cad4b10a6c90a1a0450611720cd8583
jamiesun <jamiesun.net@gmail.com>
2016-02-20 18:37:16 +0800

    version

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from fabric.api import *
 
+__version__ = '0.0.2'
+
 def tag():
-    __version__ = raw_input("release new version:")
     local("git tag -a v%s -m 'version %s'"%(__version__,__version__))
     local("git push origin v%s:v%s"%(__version__,__version__))
